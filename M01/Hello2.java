362ba53e9efc054546fbd74be113ebd8258eb42d
WyattHPearce <wpearce447@gmail.com>
2024-08-21 16:58:51 -0400

    Completed CH1 "you do it" exercises

---
+++ M01/Hello2.java
@@ -0,0 +1,16 @@
+// Filename Hello2.java
+// Written by Wyatt H. Pearce
+// Written on 8-21-2024
+
+package M01;
+
+public class Hello2 
+{
+    /* This class demonstrates the use of the println() method 
+     * to print the message Hello, world! */
+
+     public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
